87dd4081ad1edd0c24bc26bc634d6e7b12a1b391
Berkay <berkaytrhn@gmail.com>
2025-11-14 00:03:36 +0300

    ppm decoding to an opencv matrix and visualizing

--- image-file-parser/ppm_reader.cpp
+++ image-file-parser/ppm_reader.cpp
@@ -2,10 +2,12 @@
 #include <fstream>
 #include <string>
 #include <filesystem>
+#include <opencv2/opencv.hpp>
 
+using namespace cv;
 using namespace std;
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
     string tempText;
 
@@ -13,11 +15,12 @@ int main(int argc, char** argv)
     int width, height, colorDepth;
 
     // read given ppm file
-    ifstream MyFile(argv[1]);
-    // First Line 
+    ifstream MyFile(argv[1], ios::binary);
+    // First Line
     getline(MyFile, tempText);
 
-    if(tempText != "P6"){
+    if (tempText != "P6")
+    {
         cout << "Invalid PPM P6 file." << endl;
         return -1;
     }
@@ -25,7 +28,7 @@ int main(int argc, char** argv)
 
     // dimensions
     getline(MyFile, tempText);
-    
+
     stringstream stream(tempText);
     stream >> width >> height;
 
@@ -36,6 +39,21 @@ int main(int argc, char** argv)
     cout << "Image Format:" << format << endl;
     cout << "Width: " << width << ", Height: " << height << ", Color Depth: " << colorDepth << endl;
 
-    // Read image data as bytes
+    // 8 bit unsigned int, 3 channels(Order: BGR)
+    Mat image(height, width, CV_8UC3);
+    for (int i = 0; i < height; i++){
+        for (int j = 0; j < width; j++){
+        // because char 8 bits, using it for efficiency and precision
+        unsigned char r, g, b;
+        MyFile.read(reinterpret_cast<char*>(&r), 1);
+        MyFile.read(reinterpret_cast<char*>(&g), 1);
+        MyFile.read(reinterpret_cast<char*>(&b), 1);
+
+        image.at<Vec3b>(i, j) = Vec3b(b, g, r); // OpenCV uses BGR order
+        }
+    }
+
+    imshow("PPM Decoded Image", image);
+    waitKey(0);
     return 0;
 }
